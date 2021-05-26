60e30f31d6ab41ae4da154ac2ab0e47183993e05
David Gutierrez-McCormack <davidmccormack2000@gmail.com>
2021-05-25 20:38:07 -0600

    Finished part 3: Create a feature component.

---
+++ src/app/hero-details/hero-details.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Hero} from "../hero";
+
+@Component({
+  selector: 'app-hero-details',
+  templateUrl: './hero-details.component.html',
+  styleUrls: ['./hero-details.component.css']
+})
+export class HeroDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  @Input() hero ?: Hero;
+
+  ngOnInit(): void {
+  }
+
+}
